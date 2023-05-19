d793a107a248465ed1889eb83bc928f1b6208090
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-18 20:12:17 -0600

    Agregué las etiquetas de los componentes creados: banner y footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
     import Header from "$components/Header.svelte";
+    import Banner from "$components/Banner.svelte";
+    import Footer from "$components/Footer.svelte";
 </script>
 
+
 <Header />
+<Banner />
+<Footer />
