d83875cf7330323d08e08de059635a1fa3b48122
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-23 12:24:46 -0600

    Eliminé el div background que estaba rotado, porque me estaba dando problemas con el overflow

--- src/components/Experiencia.svelte
+++ src/components/Experiencia.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <div class="experiencia_container">
-    <div class="background-rotate"></div>
     <div class="experiencia-info_container">
         <h2>Trabaja con una empresa con experiencia industrial</h2>
         <h3>En RDQ, tenemos una amplia experiencia en la implementación de proyectos de automatización, lo que nos ha llevado a concretar acuerdos con empresas importantes del sector, como: CIE Matic, Valeo, Grupo AlEn, Sedena, etc.</h3>
