c9842ddbb732371fc0b9560637f02c104a990fe1
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-23 13:30:57 -0600

    Sigo haciendo cambios en el componente Marcas

---
+++ src/components/Marcas.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '$lib/styles/Marcas.css'
+    import LogoDatalogic from '$lib/images/data.png'
+    import LogoFesto from '$lib/images/fes.png'
+</script>
+
+<div class="marcas_container">
+    <div class="marcas-info_container">
+        <h2>Equipos y productos de múltiples marcas</h2>
+        <h3>En RDQ somos distribuidores autorizados de las marcas <b>Datalogic</b> y <b>Festo Electric Automation</b>, sin embargo hemos trabajado con distintas marcas del sector, como Siemens, Cognex, Balluff, Phoenix Contact, etc.</h3>
+        <h4>Distribuciones Autorizadas</h4>
+        <div class="marca-info_container">
+            <img class="img-logo" src={LogoDatalogic} alt="Logo RDQ">
+        </div>
+        <div class="marca-info_container">
+            <img class="img-logo" src={LogoFesto} alt="Logo Festo">
+        </div>
+    </div>
+</div>
