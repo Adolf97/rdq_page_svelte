60eca19f11eb5375e4b2ce4553d056e953be6062
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-23 11:44:07 -0600

    Terminé el componente de Experiencia y sus respectivos estilos

--- src/components/Experiencia.svelte
+++ src/components/Experiencia.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <div class="experiencia_container">
-    Experiencia. Hola
+    <div class="background-rotate"></div>
+    <div class="experiencia-info_container">
+        <h2>Trabaja con una empresa con experiencia industrial</h2>
+        <h3>En RDQ, tenemos una amplia experiencia en la implementación de proyectos de automatización, lo que nos ha llevado a concretar acuerdos con empresas importantes del sector, como: CIE Matic, Valeo, Grupo AlEn, Sedena, etc.</h3>
+        <button class="btn-exp">Descargar nuestro CV Empresarial</button>
+        <h3>Contamos con una oficina matriz en Querétaro, y con una oficina filial en Guanajuato.</h3>
+        <h3>Atendemos a toda la región del Bajío y Centro de México.</h3>
+        <button class="btn-contacto">Contáctanos ></button>
+    </div>
 </div>
