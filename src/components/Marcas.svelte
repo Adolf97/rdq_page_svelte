0e5d46d2925a80e19329b2b6fc5f0f1c9aab64c0
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-23 18:26:20 -0600

    Terminé el componente de Marcas, con sus respectivos estilos

--- src/components/Marcas.svelte
+++ src/components/Marcas.svelte
@@ -7,13 +7,29 @@
 <div class="marcas_container">
     <div class="marcas-info_container">
         <h2>Equipos y productos de múltiples marcas</h2>
-        <h3>En RDQ somos distribuidores autorizados de las marcas <b>Datalogic</b> y <b>Festo Electric Automation</b>, sin embargo hemos trabajado con distintas marcas del sector, como Siemens, Cognex, Balluff, Phoenix Contact, etc.</h3>
-        <h4>Distribuciones Autorizadas</h4>
+        <span>En RDQ somos distribuidores autorizados de las marcas <b>Datalogic</b> y <b>Festo Electric Automation</b>, sin embargo hemos trabajado con distintas marcas del sector, como Siemens, Cognex, Balluff, Phoenix Contact, etc.</span>
+        <h3>Distribuciones Autorizadas</h3>
         <div class="marca-info_container">
-            <img class="img-logo" src={LogoDatalogic} alt="Logo RDQ">
+            <img class="img-logoData" src={LogoDatalogic} alt="Logo RDQ">
+            <ul>
+                <li>Equipos de Trazabilidad</li>
+                <li>Equipos de sensado</li>
+                <li>Sistemas de visión</li>
+                <li>Equipos de marcado láser</li>
+                <li>Equipos de seguridad</li>
+            </ul>
+            <a href="#Productos/Datalogic" class="btn-vino">Ver todos los prodcutos ></a>
         </div>
         <div class="marca-info_container">
-            <img class="img-logo" src={LogoFesto} alt="Logo Festo">
+            <img class="img-logoFesto" src={LogoFesto} alt="Logo Festo">
+            <ul>
+                <li>Servomotores</li>
+                <li>Actuadores</li>
+                <li>Válvulas y terminales de válvulas</li>
+                <li>Motion Terminals</li>
+            </ul>
+            <a href="#Productos/Festo" class="btn-vino">Ver todos los productos ></a>
         </div>
     </div>
+    <button class="btn_quote">Solicita tu cotización</button>
 </div>
