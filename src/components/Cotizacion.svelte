85e79f3bbc3accd2fd3f47aea15a7ff6725d03b0
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-28 22:52:25 -0600

    Terminé de crear el formulario para las solicitudes de cotización

--- src/components/Cotizacion.svelte
+++ src/components/Cotizacion.svelte
@@ -6,24 +6,33 @@
     <h1>Solicita tu cotización</h1>
     <form action="">
         <div>
-            <label for="">Nombre completo:</label>
-            <input type="text" placeholder="Nombre completo">
+            <label for="">Nombre:</label>
+            <input name="name" class="cotizacion_form_box" type="text" placeholder="Nombre completo" required>
         </div>
         <div>
             <label for="">Empresa:</label>
-            <input type="text" placeholder="Nombre de tu empresa">
+            <input name="company" class="cotizacion_form_box" type="text" placeholder="Nombre de tu empresa" required>
         </div>
         <div>
             <label for="">Email:</label>
-            <input type="email" placeholder="Escribe tu email de contacto">
+            <input name="email" class="cotizacion_form_box" type="email" placeholder="Email de contacto" required>
         </div>
         <div>
             <label for="">Teléfono:</label>
-            <input type="tel" placeholder="Escribe tu teléfono de contacto">
+            <input name="tel" class="cotizacion_form_box" type="tel" placeholder="Teléfono de contacto" required>
         </div>
         <div>
             <label for="">Marca solicitada:</label>
-            <input type="text" placeholder="Nombre de tu empresa">
+            <select class="cotizacion_form_box" name="brand" id="" required>
+                <option value="datalogic">Datalogic</option>
+                <option value="festo">Festo</option>
+                <option value="others">Otra</option>
+            </select>
         </div>
+        <div>
+            <label for="">Mensaje:</label>
+            <textarea name="message" class="cotizacion_form_box" rows="4" placeholder="Escribe aquí el modelo, número de parte o una descripción del producto que estás buscando..." required></textarea>
+        </div>
+        <input type="submit" class="btn-form-submit" value="Enviar">
     </form>
 </div>
