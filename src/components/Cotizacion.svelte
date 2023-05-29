ee05d9d8da6ec4d2c5ce5dbaa5d5bd59a52dfcc4
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-28 20:25:22 -0600

    Agregué el formulario para solicitar cotización

--- src/components/Cotizacion.svelte
+++ src/components/Cotizacion.svelte
@@ -3,5 +3,27 @@
 </script>
 
 <div class="cotizacion_container">
-    Cotización
+    <h1>Solicita tu cotización</h1>
+    <form action="">
+        <div>
+            <label for="">Nombre completo:</label>
+            <input type="text" placeholder="Nombre completo">
+        </div>
+        <div>
+            <label for="">Empresa:</label>
+            <input type="text" placeholder="Nombre de tu empresa">
+        </div>
+        <div>
+            <label for="">Email:</label>
+            <input type="email" placeholder="Escribe tu email de contacto">
+        </div>
+        <div>
+            <label for="">Teléfono:</label>
+            <input type="tel" placeholder="Escribe tu teléfono de contacto">
+        </div>
+        <div>
+            <label for="">Marca solicitada:</label>
+            <input type="text" placeholder="Nombre de tu empresa">
+        </div>
+    </form>
 </div>
